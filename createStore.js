e92cd28430d27f6773eba5c4d07517dc696efd0e
Lucas <AgtLucas@users.noreply.github.com>
2016-03-27 13:34:29 -0300

    Update createStore.js

--- createStore.js
+++ createStore.js
@@ -1,22 +1,22 @@
 const createStore = (reducer) => {
-  let state;
-  let listeners = [];
+  let state
+  let listeners = []
   
-  const getState = () => state;
+  const getState = () => state
   
   const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
   }
   
   const subscribe = (listener) => {
-    listeners.push(listener);
+    listeners.push(listener)
     return () => {
-      listeners = listeners.filter(l => l !== listener);
+      listeners = listeners.filter(l => l !== listener)
     }
   }
   
-  dispatch();
+  dispatch()
   
-  return { getState, dispatch, subscribe };
+  return { getState, dispatch, subscribe }
 }
